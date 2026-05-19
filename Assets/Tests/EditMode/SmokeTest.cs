4566efb78f0866e59c31fb62bae5c15e32da4eaa
Andrey Anashkin <andexds@gmail.com>
2026-05-19 23:13:57 +0300

    test: smoke test to verify EditMode test runner works

---
+++ Assets/Tests/EditMode/SmokeTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace DrinkitGame.Tests.EditMode
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void OnePlusOne_Equals_Two()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
